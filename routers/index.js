0ffee9eda1e8911a69ba1ddc4d99e2b783c4ff79
Saurabh Dixit <smartds2550@gmail.com>
2023-05-03 18:13:12 +0000

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -230,94 +230,38 @@ router.get('/download-audio', async (req, res) => {
 
   // Set up the POST request route to convert the YouTube link to video
 
-// router.post('/convert-video', async (req, res) => {
-//   try{
-//     const url = req.body.url;
-//     if(!ytdl.validateURL(url)){
-//       return res.render('VideoConverter',{
-//         video: null,
-//         title: 'Video Converter And Downloader | Youtube Converter',
-//         message: 'Please Enter A Valid Youtube URL'
-      
-//       });
-//     }
-//     const info = await ytdl.getInfo(url);
-//     const video = {
-//       title: info.videoDetails.title,
-//       url: url,
-//       thumbnail: info.videoDetails.thumbnails[0].url,
-//       // formats: info.formats.filter(format => format.container === 'mp4')
-//       formats: videoFormats.map(format => {
-//         return {
-//           quality: format.qualityLabel,
-//           itag: format.itag,
-//           type: format.mimeType,
-//           url: format.url,
-//         };
-//       })
-//     };
-//     return res.render('VideoConverter', { 
-//       video: video,
-//       title: 'Video Converter And Downloader | Youtube Converter',
-//       message: 'Converted Successfully , Please Download'
-//     });
-//   }catch(error){
-//     console.log('Error Video Converting:' ,error);
-//     return res.render('VideoConverter', { 
-//       video: null,
-//       title: 'Video Converter And Downloader | Youtube Converter',
-//       message: `'Error When Converting' ,${error.message}`
-//     });
-//   }
-// });  // main without formats map
-
-
 router.post('/convert-video', async (req, res) => {
   try{
-      const url = req.body.url;
-      if (!ytdl.validateURL(url)) {
-        return res.render('VideoConverter', {
-          video: null,
-          title: 'Video Converter And Downloader | Youtube Converter',
-          message: 'Please Enter A Valid Youtube URL'
-        });
-      }
-      const info = await ytdl.getInfo(url);
-      const video = {
-        title: info.videoDetails.title,
-        url: url,
-        thumbnail: info.videoDetails.thumbnails[0].url,
-        formats: info.formats.filter(format => format.container === 'mp4')
-      };
-
-      const availableQualities = ["2160p", "1440p", "1080p", "720p", "480p", "360p"];
-      const filteredFormats = [];
-      availableQualities.forEach((quality) => {
-        const format = video.formats.find(f => f.qualityLabel === quality);
-        if (format) filteredFormats.push(format);
-      });
-      video.formats = filteredFormats;
-
-      return res.render('VideoConverter', {
-        video: video,
-        title: 'Video Converter And Downloader | Youtube Converter',
-        message: 'Converted Successfully in all available qualities, Please Download'
-      });
-    }catch(error){
-      console.log('Error Video Converting:' ,error);
-      return res.render('VideoConverter', { 
+    const url = req.body.url;
+    if(!ytdl.validateURL(url)){
+      return res.render('VideoConverter',{
         video: null,
         title: 'Video Converter And Downloader | Youtube Converter',
-        message: `'Error When Converting' ,${error.message}`
+        message: 'Please Enter A Valid Youtube URL'
+      
       });
     }
-  });
-
-
-
-
-
-
+    const info = await ytdl.getInfo(url);
+    const video = {
+      title: info.videoDetails.title,
+      url: url,
+      thumbnail: info.videoDetails.thumbnails[0].url,
+      formats: info.formats.filter(format => format.container === 'mp4')
+    };
+    return res.render('VideoConverter', { 
+      video: video,
+      title: 'Video Converter And Downloader | Youtube Converter',
+      message: 'Converted Successfully , Please Download'
+    });
+  }catch(error){
+    console.log('Error Video Converting:' ,error);
+    return res.render('VideoConverter', { 
+      video: null,
+      title: 'Video Converter And Downloader | Youtube Converter',
+      message: `'Error When Converting' ,${error.message}`
+    });
+  }
+});  // main without formats map
 
 
   // Set up the GET request route to download the selected video format
